4b7efc24be03b4ce9da579a4d8c2954ff0c780e8
Jeff Mueller <jeff@jandmlab.com>
2015-10-20 15:35:57 -0700

    Added comments, constraint for small screen sizes

--- app/themes/odfw-ocs-sage/assets/scripts/search-box.js
+++ app/themes/odfw-ocs-sage/assets/scripts/search-box.js
@@ -1,17 +1,27 @@
 $(document).ready(function() {
-  var first_h1 = $('h1').text();
-  var page_title = "[CONSERVE] [" + first_h1 + "]";
+  // We only want to change the contents of the search box if we're on
+  // a device bigger than a phone
+  if ($(window).width() > 767) {
+    // Grab the content of the first h1 on the page and create
+    // the search box's placeholder
+    var first_h1 = $('h1').text();
+    var page_title = "[Conserve] [" + first_h1 + "]";
 
-  $('#search-field').attr('placeholder', page_title);
+    // Set the placeholder
+    $('#search-field').attr('placeholder', page_title.toUpperCase());
 
-  $('#search-field').on('focus', function() {
-    $(this).attr('placeholder', '');
-  });
+    // When the user clicks in the box, remove the placeholder
+    $('#search-field').on('focus', function() {
+      $(this).attr('placeholder', '');
+    });
 
-  $('#search-field').on('blur', function() {
-    var text_length = $(this).val().length;
-    if (text_length === 0) {
-      $(this).attr('placeholder', page_title);
-    }
-  });
+    // When the user clicks out of the box, reset the placeholder if the user
+    // hasn't entered in any text
+    $('#search-field').on('blur', function() {
+      var text_length = $(this).val().length;
+      if (text_length === 0) {
+        $(this).attr('placeholder', page_title);
+      }
+    });
+  }
 });
