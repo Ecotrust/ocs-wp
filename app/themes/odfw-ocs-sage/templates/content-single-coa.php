380aec279d45cddeded1fc870f95d92ac1c5d5e5
Drew <drew@pointnineseven.com>
2015-11-09 12:04:06 -0800

    cleanup unnecessary html tags for compass

--- app/themes/odfw-ocs-sage/templates/content-single-coa.php
+++ app/themes/odfw-ocs-sage/templates/content-single-coa.php
@@ -6,19 +6,17 @@
 
     <?php $the_compass_field = get_post_meta( get_the_ID(), 'coa_meta_compass-link', true );
         if ( ! empty($the_compass_field) ): ?>
-            <div class="compass main">
-                <div class="compass-frame">                   
-                    <span class="compass-close">
-                        <i class="glyphicon glyphicon-remove-sign"></i>
-                    </span>
-                    <div class="view-compass">
-                        <a href="<?php echo external_odfw_compass_url($the_compass_field) ?>"  target="_blank">
-                            <i class="glyphicon glyphicon-dashboard"></i> 
-                            VIEW DATA LAYERS IN COMPASS
-                        </a>
-                    </div>
-                    <?php the_odfw_compass_iframe($the_compass_field); ?>
+            <div class="compass main">                 
+                <span class="compass-close">
+                    <i class="glyphicon glyphicon-remove-sign"></i>
+                </span>
+                <div class="view-external-compass">
+                    <a href="<?php echo external_odfw_compass_url($the_compass_field) ?>"  target="_blank">
+                        <i class="glyphicon glyphicon-dashboard"></i> 
+                        VIEW DATA LAYERS IN COMPASS
+                    </a>
                 </div>
+                <?php the_odfw_compass_iframe($the_compass_field); ?>
             </div>
     <?php endif; ?>
     
