c76bb8dcd7e54694a88568bc5de7d3ec82e502de
Drew <drew@pointnineseven.com>
2015-11-16 16:19:14 -0800

    only run svgPopup when it exists

--- app/themes/odfw-ocs-sage/assets/scripts/svgPopup.js
+++ app/themes/odfw-ocs-sage/assets/scripts/svgPopup.js
@@ -3,31 +3,32 @@ jQuery(document).ready(function($) {
        $('button.view-map').click(function(){
             setTimeout(function() {
                 var svg = document.getElementById('regions');
-                var svgDoc = svg.contentDocument;
+                if (svg != null) {
+                    var svgDoc = svg.contentDocument;
 
-                //loop through each key-value pair and assign contents to associated item in svg
-                $.each(svg_popup_vars, function(key, value) {
-                    var str = key.replace(/\s+/g, '-').toLowerCase();
-                    $(svgDoc).find('a#'+str).attr({
-                        'data-toggle': 'popover', 
-                        'data-trigger': 'hover focus',
-                        'data-original-title': key, 
-                        'data-content': value
+                    //loop through each key-value pair and assign contents to associated item in svg
+                    $.each(svg_popup_vars, function(key, value) {
+                        var str = key.replace(/\s+/g, '-').toLowerCase();
+                        $(svgDoc).find('a#'+str).attr({
+                            'data-toggle': 'popover', 
+                            'data-trigger': 'hover focus',
+                            'data-original-title': key, 
+                            'data-content': value
+                        });
                     });
-                });
 
-                $(svgDoc).find('[data-toggle="popover"]').popover({
-                    'container':'.ecoregion-svg',
-                    'html': true,
-                });
+                    $(svgDoc).find('[data-toggle="popover"]').popover({
+                        'container':'.ecoregion-svg',
+                        'html': true,
+                    });
 
-                $(svgDoc).mousemove(function(event) { 
-                    var left = event.pageX + 10; 
-                    var top = event.pageY - 65;
-                    $('.popover').css({top: top,left: left}).show();
-                });
-            }, 750) 
-
+                    $(svgDoc).mousemove(function(event) { 
+                        var left = event.pageX + 10; 
+                        var top = event.pageY - 65;
+                        $('.popover').css({top: top,left: left}).show();
+                    });
+                }
+            }, 750);
        });
     }
 });
