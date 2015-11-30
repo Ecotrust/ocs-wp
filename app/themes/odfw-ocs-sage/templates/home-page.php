c7fc2364442523a3e78dc0c8c52721bab53768b9
Drew <drew@pointnineseven.com>
2015-11-30 14:37:38 -0800

    add initial makeup for oregon mask

---
+++ app/themes/odfw-ocs-sage/templates/home-page.php
@@ -0,0 +1,10 @@
+<div class="svg-mask">
+    <svg width="100%" height="100%" baseProfile="full" version="1.2">
+        <defs>
+            <mask id="svgmask2" maskUnits="userSpaceOnUse" maskContentUnits="userSpaceOnUse" transform="scale(1)">
+                <image width="100%" height="100%" xlink:href="app/themes/odfw-ocs-sage/dist/images/or.png" />
+            </mask>
+        </defs>
+        <image id="the-mask" mask="url(#svgmask2)" width="100%" height="100%" y="0" x="0" xlink:href="" />
+    </svg>
+</div>
