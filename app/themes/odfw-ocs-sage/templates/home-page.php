937ebb09963939491bc0142d1f09069a13b5289b
Drew <drew@pointnineseven.com>
2015-12-03 08:49:44 -0800

    use bootstrap carousel for slider functionality

--- app/themes/odfw-ocs-sage/templates/home-page.php
+++ app/themes/odfw-ocs-sage/templates/home-page.php
@@ -1,10 +1,74 @@
-<div class="svg-mask">
-    <svg width="100%" height="100%" baseProfile="full" version="1.2">
-        <defs>
-            <mask id="svgmask2" maskUnits="userSpaceOnUse" maskContentUnits="userSpaceOnUse" transform="scale(1)">
-                <image width="100%" height="100%" xlink:href="app/themes/odfw-ocs-sage/dist/images/or.png" />
-            </mask>
-        </defs>
-        <image id="the-mask" mask="url(#svgmask2)" width="100%" height="100%" y="0" x="0" xlink:href="" />
-    </svg>
+
+<div class="svg-mask carousel slide" id="myCarousel" data-ride="carousel">
+
+     <div class="carousel-inner" role="listbox">
+        
+        <div class="item active">
+            <div class="carousel-caption">
+                <h3 class="carousel-headline">TESTfghfghfgh dh fjfgh  fghfhgf</h3>
+                <p class="carousel-subtext">Captions gch fghfdh gg;dflkjdf.kgj glkjfcg/.dfhg /;fdjkhdf/lhkf dg df/g.d f/gldf/ gl kdf'glkdfglkdjfg/;ldkf'gl;d;l</p>
+            </div>
+
+          <svg width="100%" height="100%" baseProfile="full" version="1.2">
+              <defs>
+                  <mask id="svgmask2" maskUnits="userSpaceOnUse" maskContentUnits="userSpaceOnUse" transform="scale(1)">
+                      <image width="100%" height="100%" xlink:href="app/themes/odfw-ocs-sage/dist/images/or.png" />
+                  </mask>
+              </defs>        
+              <image id="the-mask" mask="url(#svgmask2)" width="100%" height="100%" y="0" x="0" xlink:href="https://upload.wikimedia.org/wikipedia/commons/3/3d/LARGE_elevation.jpg" />
+          </svg>
+
+        </div>
+
+        <div class="item">
+          <svg width="100%" height="100%" baseProfile="full" version="1.2">
+              <defs>
+                  <mask id="svgmask2" maskUnits="userSpaceOnUse" maskContentUnits="userSpaceOnUse" transform="scale(1)">
+                      <image width="100%" height="100%" xlink:href="app/themes/odfw-ocs-sage/dist/images/or.png" />
+                  </mask>
+              </defs>
+              <image id="the-mask" mask="url(#svgmask2)" width="100%" height="100%" y="0" x="0" xlink:href="http://wowslider.com/sliders/demo-85/data1/images/southtyrol350698.jpg" />
+          </svg>
+        </div>
+
+        <div class="item">
+          <svg width="100%" height="100%" baseProfile="full" version="1.2">
+              <defs>
+                  <mask id="svgmask2" maskUnits="userSpaceOnUse" maskContentUnits="userSpaceOnUse" transform="scale(1)">
+                      <image width="100%" height="100%" xlink:href="app/themes/odfw-ocs-sage/dist/images/or.png" />
+                  </mask>
+              </defs>
+              <image id="the-mask" mask="url(#svgmask2)" width="100%" height="100%" y="0" x="0" xlink:href="https://upload.wikimedia.org/wikipedia/commons/4/43/Very_Large_Array,_2012.jpg" />
+          </svg>
+
+        </div>
+
+        <div class="item">
+          <svg width="100%" height="100%" baseProfile="full" version="1.2">
+              <defs>
+                  <mask id="svgmask2" maskUnits="userSpaceOnUse" maskContentUnits="userSpaceOnUse" transform="scale(1)">
+                      <image width="100%" height="100%" xlink:href="app/themes/odfw-ocs-sage/dist/images/or.png" />
+                  </mask>
+              </defs>
+              <image id="the-mask" mask="url(#svgmask2)" width="100%" height="100%" y="0" x="0" xlink:href="http://www.planwallpaper.com/static/images/beautiful-sunset-images-196063.jpg" />
+          </svg>
+        </div>
+
+      </div>
+
+
+      <a href="#myCarousel" class="control_next" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      </a>
+      <a href="#myCarousel" class="control_prev" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      </a>
+
+      <ol class="carousel-indicators">
+         <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+         <li data-target="#myCarousel" data-slide-to="1"></li>
+         <li data-target="#myCarousel" data-slide-to="2"></li>
+         <li data-target="#myCarousel" data-slide-to="3"></li>
+      </ol>
+
 </div>
