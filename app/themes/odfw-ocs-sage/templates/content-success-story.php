e88b333d3a5ac22ff118f362e78c44d45a11ada0
Will Moore <wmoore@ecotrust.org>
2015-12-06 23:19:53 -0800

    Check for Success Story Before Using it
    Fixes #51

--- app/themes/odfw-ocs-sage/templates/content-success-story.php
+++ app/themes/odfw-ocs-sage/templates/content-success-story.php
@@ -1,12 +1,20 @@
 <?php
-  $success_story_post_id = get_post_meta(get_the_ID(), 'success_story', true);
-  if ($success_story_post_id) {
-    $success_story = get_post($success_story_post_id);
+$success_story_post_id = get_post_meta(get_the_ID(), 'success_story', true);
+if ($success_story_post_id):
+	$success_story = get_post($success_story_post_id);
+	if ($success_story):
 ?>
-    <aside class="success-story" name="success-story">
-    <h2><?php echo $success_story->post_title; ?></h2>
-      <div class="success-story-content">
-        <?php echo wpautop($success_story->post_content); ?>
-      </div>
-    </aside>
-<?php } ?>
+
+		<aside class="success-story" name="success-story">
+			<?php //@TODO H2 only if it's at the bottom of the page? -
+				// they shouldn't be in the sidebar nav except the final one ?>
+			<h2><?php echo $success_story->post_title; ?></h2>
+			<div class="success-story-content">
+				<?php echo wpautop($success_story->post_content); ?>
+			</div>
+		</aside>
+
+<?php
+	endif;
+endif;
+?>
