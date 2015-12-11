7537364708f8f68092bf391ff4158a0b1e335ae6
Drew <drew@pointnineseven.com>
2015-12-11 15:56:52 -0800

    show info glyph on featured-thumbs only if caption or attribution is available

--- app/themes/odfw-ocs-sage/templates/featured-thumbnail.php
+++ app/themes/odfw-ocs-sage/templates/featured-thumbnail.php
@@ -1,13 +1,25 @@
 <?php if ( has_post_thumbnail() ) : ?>
+    <?php
+        $thumbnail = get_post( get_post_thumbnail_id() );
+        $caption = $thumbnail->post_excerpt;
+        $attrName = get_post_meta( $thumbnail->ID, 'odfw_attribution_name', true );
+    ?>
     <figure class="feature-thumb">
         <div class="image-container">
-            <span class="photo-info show-info glyphicon glyphicon-info-sign"></span>
+            <?php if ( $caption || $attrName ) : ?>
+                <span class="photo-info show-info glyphicon glyphicon-info-sign"></span>
+            <?php endif; ?>
             <?php echo the_post_thumbnail('large') ?>
         </div>
 
         <figcaption>
             <span class="photo-info glyphicon glyphicon-remove-circle"></span>
-            <?php echo get_post( get_post_thumbnail_id() )->post_excerpt; ?>
+            <?php if ( $caption ) : ?>
+                <?php echo $caption; ?>, 
+            <?php endif; ?>
+            <?php if ( $attrName ) : ?>
+                <?php echo $attrName; ?> 
+            <?php endif; ?>
         </figcaption>
     </figure>
 <?php endif; ?>
