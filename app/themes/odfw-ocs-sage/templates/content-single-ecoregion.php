e60729d13c4ff4fea8ea05762b642135283dae97
Will Moore <wmoore@ecotrust.org>
2015-12-11 05:45:00 -0800

    Typography Tweaks
    Related to #26

--- app/themes/odfw-ocs-sage/templates/content-single-ecoregion.php
+++ app/themes/odfw-ocs-sage/templates/content-single-ecoregion.php
@@ -7,13 +7,13 @@
     <?php $the_compass_field = get_post_meta( get_the_ID(), 'ecoregion_meta_compass-link', true );
         if ( ! empty($the_compass_field) ): ?>
         	<div class="compass main">
-				<div class="compass-container">                 
+				<div class="compass-container">
 				    <span class="compass-close">
 				        <i class="glyphicon glyphicon-remove-circle"></i>
 				    </span>
 				    <div class="view-external-compass">
 				        <a href="<?php echo external_odfw_compass_url($the_compass_field) ?>"  target="_blank">
-				            <i class="glyphicon glyphicon-dashboard"></i> 
+				            <i class="glyphicon glyphicon-dashboard"></i>
 				            VIEW DATA LAYERS IN COMPASS
 				        </a>
 				    </div>
@@ -24,19 +24,19 @@
 
     <div class="entry-content">
     	<?php get_template_part('templates/featured-thumbnail'); ?>
+		<h2>Description</h2>
 		<?php the_content(); ?>
 
 
 		<section class="ecoregion-characteristics-title">
-			<?php //<h3>"At a glance" Characteristics and Statistics</h3> ?>
-			<h2>Description</h2>
+			<h2>Characteristics</h3>
 
 
 
 <?php // @TODO Needs better formatting. Table or DL or something ?>
 
 			<section class="cmb2-wrap-textarea_small ecoregion_meta_important-industries">
-				<h3>Important industries</h3>
+				<h5>IMPORTANT INDUSTRIES</h5>
 
 				<p class="cmb2-textarea_small">
 					<?php $the_field = get_post_meta( get_the_ID(), 'ecoregion_meta_important-industries', true );
@@ -47,7 +47,7 @@
 
 
 			<section class="cmb2-wrap-textarea_small ecoregion_meta_major-crops">
-				<h3>Major crops</h3>
+				<h5>MAJOR CROPS</h5>
 
 				<p class="cmb2-textarea_small">
 					<?php $the_field = get_post_meta( get_the_ID(), 'ecoregion_meta_major-crops', true );
@@ -58,7 +58,7 @@
 
 
 			<section class="cmb2-wrap-textarea_small ecoregion_meta_recreational-areas">
-				<h3>Important nature-based recreational areas</h3>
+				<h5>IMPORTANT NATURE-BASED RECREATIONAL AREAS</h5>
 
 				<p class="cmb2-textarea_small">
 					<?php $the_field = get_post_meta( get_the_ID(), 'ecoregion_meta_recreational-areas', true );
@@ -69,7 +69,7 @@
 
 
 			<section class="cmb2-wrap-text ecoregion_meta_elevation">
-				<h3>Elevation</h3>
+				<h5>ELEVATION</h5>
 
 				<p class="cmb2-text">
 					<?php $the_field = get_post_meta( get_the_ID(), 'ecoregion_meta_elevation', true );
@@ -80,7 +80,7 @@
 
 
 			<section class="cmb2-wrap-text ecoregion_meta_number-of-species">
-				<h3>Number of vertebrate wildlife species</h3>
+				<h5>NUMBER OF VERTEBRATE WILDLIFE SPECIES</h5>
 
 				<p class="cmb2-text">
 					<?php $the_field = get_post_meta( get_the_ID(), 'ecoregion_meta_number-of-species', true );
@@ -91,7 +91,7 @@
 
 
 			<section class="cmb2-wrap-textarea_small ecoregion_meta_rivers">
-				<h3>Important rivers</h3>
+				<h5>IMPORTANT RIVERS</h5>
 
 				<p class="cmb2-textarea_small">
 					<?php $the_field = get_post_meta( get_the_ID(), 'ecoregion_meta_rivers', true );
@@ -102,7 +102,7 @@
 
 
 			<section class="cmb2-wrap-textarea_small ecoregion_meta_outstanding">
-				<h3>Ecologically outstanding areas</h3>
+				<h5>ECOLOGICALLY OUTSTANDING AREAS</h5>
 
 				<p class="cmb2-textarea_small">
 					<?php $the_field = get_post_meta( get_the_ID(), 'ecoregion_meta_outstanding', true );
@@ -115,18 +115,12 @@
 
 
 		<section class="cmb2-wrap-title ecoregion_meta_ecoregions">
-			<h3>Conservation Issues and Priorities</h3>
+			<h2>Conservation Issues and Priorities</h2>
 
 			<p class="cmb2-title">
 				<?php $the_field = get_post_meta( get_the_ID(), 'ecoregion_meta_ecoregions', true );
 				echo esc_html( $the_field ); ?>
 			</p>
-		</section>
-
-
-
-		<section class="cmb2-wrap-wysiwyg ecoregion_meta_conservation-issues-overview">
-			<h3>Overview</h3>
 
 			<div class="cmb2-wysiwyg">
 				<?php $the_field = get_post_meta( get_the_ID(), 'ecoregion_meta_conservation-issues-overview', true );
@@ -145,10 +139,9 @@
 				<?php $the_field = get_post_meta( get_the_ID(), 'factors_repeat_group', true );
 
 				foreach($the_field as $entries => $entry ) { ?>
-					<h3><?php echo $entry['ecoregion_meta_factor_title'];?></h3>
-					<h4>Description:</h4>
+					<h3>Limiting Factor: <?php echo $entry['ecoregion_meta_factor_title'];?></h3>
 					<p><?php echo $entry['ecoregion_meta_factor_description'];?></p>
-					<h4>Approach</h4>
+					<h5>Recommended Approach</h5>
 					<p><?php echo $entry['ecoregion_meta_approach'];?></p>
 
 				<?php } ?>
