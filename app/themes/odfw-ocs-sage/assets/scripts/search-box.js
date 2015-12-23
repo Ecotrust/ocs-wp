76c1840e1cf8b71ef668d55eb00c4121bf0ccd6d
Will Moore <wmoore@ecotrust.org>
2015-12-23 06:27:16 -0800

    WIP on Adjustable Search Box Font Size

--- app/themes/odfw-ocs-sage/assets/scripts/search-box.js
+++ app/themes/odfw-ocs-sage/assets/scripts/search-box.js
@@ -13,8 +13,76 @@ function checkSearchBoxLength (){
  * [1] http://fittextjs.com/
  * [1] http://stackoverflow.com/questions/687998/auto-size-dynamic-text-to-fill-fixed-size-container )
  */
+
+
+/*
+    var $search_field = $('#search-field'),
+		curVal = $search_field.val(),
+		curValLen = curVal.length,
+		inputClass = "";
+
+
+	//TODO Smaller/easier:
+	// default size and an over 60 size?
+	if ( curValLen > 40 && curValLen < 65 ) {
+		inputClass = "search-40-65";
+	} else if ( curValLen > 65 && curValLen < 90 ) {
+		inputClass = "search-65-90";
+	} else if ( curValLen > 90 && curValLen < 115 ) {
+		inputClass = "search-90-115";
+	} else if ( curValLen > 115 ) {
+		inputClass = "search-over-115";
+	}
+
+	$search_field.addClass(inputClass);
+
+	*/
+
 }
 
+/*global jQuery */
+/*!
+* FitText.js 1.2
+*
+* Copyright 2011, Dave Rupert http://daverupert.com
+* Released under the WTFPL license
+* http://sam.zoy.org/wtfpl/
+*
+* Date: Thu May 05 14:23:00 2011 -0600
+*/
+
+(function( $ ){
+
+  $.fn.fitText = function( kompressor, options ) {
+
+    // Setup options
+    var compressor = kompressor || 1,
+        settings = $.extend({
+          'minFontSize' : Number.NEGATIVE_INFINITY,
+          'maxFontSize' : Number.POSITIVE_INFINITY
+        }, options);
+
+    return this.each(function(){
+
+      // Store the object
+      var $this = $(this);
+
+      // Resizer() resizes items based on the object width divided by the compressor * 10
+      var resizer = function () {
+        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+      };
+
+      // Call once to set.
+      resizer();
+
+      // Call on resize. Opera debounces their resize by default.
+      $(window).on('resize.fittext orientationchange.fittext', resizer);
+
+    });
+
+  };
+
+})( jQuery );
 
 $(document).ready(function() {
   // We only want to change the contents of the search box if we're on
@@ -31,6 +99,8 @@ $(document).ready(function() {
 
     // Set the placeholder
     $search_field.attr('placeholder', page_title.toUpperCase()).addClass('focused');
+
+	$search_field.on('input', checkSearchBoxLength );
 
     //@TODO: check length of content and resize text to fit
     checkSearchBoxLength();
