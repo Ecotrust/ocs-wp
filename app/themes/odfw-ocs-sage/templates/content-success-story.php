83fcbcc9e4df06fb58b7a764e4247e2568e1b1e5
Drew <drew@pointnineseven.com>
2015-12-30 14:53:54 -0800

    add featured images to success story asides

--- app/themes/odfw-ocs-sage/templates/content-success-story.php
+++ app/themes/odfw-ocs-sage/templates/content-success-story.php
@@ -8,6 +8,33 @@ if ($success_story_post_id):
 
 		<aside class="success-story" name="success-story">
 			<h2><?php echo $success_story->post_title; ?></h2>
+
+            <?php if ( has_post_thumbnail($success_story_post_id) ) : ?>
+                <?php
+                    $thumbnail = get_post( get_post_thumbnail_id($success_story_post_id) );
+                    $caption = $thumbnail->post_excerpt;
+                    $attrName = get_post_meta( $thumbnail->ID, 'odfw_attribution_name', true );
+                ?>
+                <figure class="feature-thumb">
+                    <div class="image-container">
+                        <?php if (!empty($caption) || !empty($attrName)): ?>
+                            <span class="photo-info show-info glyphicon glyphicon-info-sign"></span>
+                        <?php endif; ?>
+                        <?php echo get_the_post_thumbnail($success_story_post_id, 'large') ?>
+                    </div>
+
+                    <figcaption>
+                        <span class="photo-info glyphicon glyphicon-remove-circle"></span>
+                        <?php if ( !empty($caption) ) : ?>
+                            <?php echo $caption; ?> 
+                        <?php endif; ?>
+                        <?php if ( !empty($attrName) ) : ?>
+                            Photo Credit: <?php echo $attrName; ?> 
+                        <?php endif; ?>
+                    </figcaption>
+                </figure>
+            <?php endif; ?>
+
 			<div class="success-story-content">
 				<?php echo wpautop($success_story->post_content); ?>
 			</div>
