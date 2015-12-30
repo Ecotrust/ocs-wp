3a4ec5b58c5fe1ce2ce4de2a784e324b501dedd4
Drew <drew@pointnineseven.com>
2015-12-30 13:26:57 -0800

    update 404 and no-result search pages

--- app/themes/odfw-ocs-sage/404.php
+++ app/themes/odfw-ocs-sage/404.php
@@ -1,7 +1,8 @@
 <?php get_template_part('templates/page', 'header'); ?>
 
-<div class="alert alert-warning">
-  <?php _e('Sorry, but the page you were trying to view does not exist.', 'sage'); ?>
-</div>
-
-<?php get_search_form(); ?>
+<h3>Sorry, but the page you were trying to view does not exist.</h3>
+<ul>
+    <li>Double check the address you entered</li>
+    <li>Try searching the Oregon Conservation Strategy above</li>
+    <li>Return to the <a href="/">homepage</a></li>
+</ul>
--- app/themes/odfw-ocs-sage/search.php
+++ app/themes/odfw-ocs-sage/search.php
@@ -1,10 +1,12 @@
 <?php get_template_part('templates/page', 'header'); ?>
 
 <?php if (!have_posts()) : ?>
-  <div class="alert alert-warning">
-    <?php _e('Sorry, no results were found.', 'sage'); ?>
-  </div>
-  <?php get_search_form(); ?>
+  <h3>No results matched your search terms.</h3>
+  <ul>
+      <li>Make sure all words are spelled correctly</li>
+      <li>Simplify your search: try fewer or more general keywords</li>
+      <li>Use keywords that reflect words or phrases likely to be in the text of the item you are seeking</li>
+  </ul>
 <?php else: ?>
     <h1 class="search-results">SEARCH RESULTS:</h1>
 <?php endif; ?>
