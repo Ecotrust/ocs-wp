82d0e3ae546f9a477526e06c3a2349fddfcc06f8
Will Moore <wmoore@ecotrust.org>
2016-03-07 18:18:54 -0800

    Add Thumbs to COA Listings. Remove List View for Species Listings

--- app/themes/odfw-ocs-sage/templates/cpt-parts/part-coa.php
+++ app/themes/odfw-ocs-sage/templates/cpt-parts/part-coa.php
@@ -3,11 +3,10 @@
 			<?php //get_template_part('templates/featured-thumbnail'); ?>
 				<?php if ( has_post_thumbnail($post->ID) ) : ?>
 					<div class="image-grid-container">
-						<?php //echo get_the_post_thumbnail($post->ID, 'grid') ?>
+						<?php echo get_the_post_thumbnail($post->ID, 'grid') ?>
 					</div>
 				<?php endif; ?>
 
-				<?php //$incAssoc =  get_query_var('coa-assoc', false); ?>
 				<?php $coa_id = get_post_meta( get_the_ID(), 'coa_meta_coa_id', true );?>
 
 				<h3 class="cpt-title"><?php the_title(); ?>
