b78a806e6a1387969a5d43095fff749c56f03928
Will Moore <wmoore@ecotrust.org>
2016-03-21 15:45:28 -0700

    Add Splash Page

---
+++ app/themes/odfw-ocs-sage/maintenance.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Oregon Conservation Strategy</title>
+<style>
+	html, body {
+		min-height: 100%;
+	}
+	body {
+		background: #005130 url("http://oregonconservationstrategy.org/app/themes/odfw-ocs-sage/assets/images/oregon-green.png") no-repeat scroll 50% 50% / 80% auto;
+		text-align: center;
+	}
+	h1,h2,h3,h4,h5,h6, p{
+		color:#414041;
+		color: #ffffff;
+		font-family:"TradeGothicBold","HelveticaNeue",Helvetica,Arial,sans-serif;
+		font-weight:500;
+		line-height:1.1;
+		margin-left: auto;
+		margin-right: auto;
+		max-width: 65%;
+	}
+	h1 {
+		font-size: 2em;
+		font-weight: bold;
+	}
+	section {
+		text-align: center;
+		width: 70vw;
+
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+
+</style>
+</head>
+
+<body class="home page">
+
+<section>
+	<img src="http://oregonconservationstrategy.org/app/themes/odfw-ocs-sage/dist/images/logo-odfw-with-text_sm@2x.png" alt="Oregon Conservation Strategy">
+	<p>We're working on an update to the 2005 Oregon Conservation Strategy. Watch this space!</p>
+</section>
+
+</body>
+</html>
