113682576654ac9ac48203c6c8ba743ed68f5f86
Will Moore <wmoore@ecotrust.org>
2016-03-31 12:24:11 -0700

    Maintenance page tweaks

--- app/themes/odfw-ocs-sage/maintenance.php
+++ app/themes/odfw-ocs-sage/maintenance.php
@@ -4,35 +4,39 @@
 <meta charset="UTF-8">
 <title>Oregon Conservation Strategy</title>
 <style>
+
+
+
 	html, body {
 		min-height: 100%;
 	}
 	body {
-		background: #005130 url("http://oregonconservationstrategy.org/app/themes/odfw-ocs-sage/assets/images/oregon-green.png") no-repeat scroll 50% 50% / 80% auto;
-		text-align: center;
+		background: #005130 url("http://oregonconservationstrategy.org/app/themes/odfw-ocs-sage/assets/images/oregon-green.png");
+		background-position: center 7%;
+		background-repeat: no-repeat;
+	    background-size: 60% auto;
+		padding-top: 50%;
 	}
 	h1,h2,h3,h4,h5,h6, p{
 		color:#414041;
 		color: #ffffff;
-		font-family:"TradeGothicBold","HelveticaNeue",Helvetica,Arial,sans-serif;
+		font-family:"TradeGothicBold","Helvetica Neue",Helvetica,Arial,sans-serif;
 		font-weight:500;
-		line-height:1.1;
+		line-height:1.4;
 		margin-left: auto;
 		margin-right: auto;
-		max-width: 65%;
+		max-width: 85%;
 	}
 	h1 {
 		font-size: 2em;
 		font-weight: bold;
 	}
+	p {
+		font-size: 1.2em;
+		font-weight: 200;
+	}
 	section {
 		text-align: center;
-		width: 70vw;
-
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -50%);
 	}
 	a {
 		color: #cceecc;
@@ -41,13 +45,34 @@
 		color: #aaeeaa;
 	}
 
+	/* Large Screens Only */
+	@media (min-width:668px){
+		body {
+			background-position: 50% 50%;
+		    background-size: 80% auto;
+			padding-top: 0;
+		}
+		h1,h2,h3,h4,h5,h6, p{
+			max-width: 65%
+		}
+		section {
+			width: 70vw;
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+		}
+
+	}
 </style>
 </head>
 
 <body class="home page">
 
 <section>
-	<img src="http://oregonconservationstrategy.org/app/themes/odfw-ocs-sage/dist/images/logo-odfw-with-text_sm@2x.png" alt="Oregon Conservation Strategy">
+	<a href="http://www.dfw.state.or.us/conservationstrategy/">
+		<img src="http://oregonconservationstrategy.org/app/themes/odfw-ocs-sage/dist/images/logo-odfw-with-text_sm@2x.png" alt="Oregon Conservation Strategy">
+	</a>
 	<p>We're working on an update to the 2005 Oregon Conservation Strategy. Watch this space!</p>
 </section>
 
