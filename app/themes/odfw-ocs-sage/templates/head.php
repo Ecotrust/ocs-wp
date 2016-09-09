294152c1462c80e82fd6cdca84f66761a529bbff
Drew <drew@pointnineseven.com>
2016-09-08 17:11:47 -0700

    add google console verification

--- app/themes/odfw-ocs-sage/templates/head.php
+++ app/themes/odfw-ocs-sage/templates/head.php
@@ -2,6 +2,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="google-site-verification" content="nGfWTwdYO4auXg8ACjTzBCp-s4ewVw9SW5mwRsbA3-s" />
 	<script>
 		var ocs_asset_dir = "<?php echo get_stylesheet_directory_uri(); ?>";
 		<?php // try to prevent FOUC for page title/search box ?>
