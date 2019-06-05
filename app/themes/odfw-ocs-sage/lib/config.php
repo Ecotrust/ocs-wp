266cbe1ffce8c897d7adea94e438d4e29a310a41
David Pollard <pollardld@gmail.com>
2019-06-04 17:29:33 -0700

    Remove jquery hosted on google api

--- app/themes/odfw-ocs-sage/lib/config.php
+++ app/themes/odfw-ocs-sage/lib/config.php
@@ -11,7 +11,7 @@ add_theme_support('soil-clean-up');         // Enable clean up from Soil
 //add_theme_support('soil-nav-walker');       // Enable cleaner nav walker from Soil
 add_theme_support('soil-relative-urls');    // Enable relative URLs from Soil
 add_theme_support('soil-nice-search');      // Enable nice search from Soil
-add_theme_support('soil-jquery-cdn');       // Enable to load jQuery from the Google CDN
+// add_theme_support('soil-jquery-cdn');       // Enable to load jQuery from the Google CDN
 
 /**
  * Configuration values
