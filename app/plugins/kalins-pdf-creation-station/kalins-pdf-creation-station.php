0cd73f1939efc9870883c38beb56762c48a60c47
David Pollard <pollardld@gmail.com>
2019-07-02 13:58:03 -0600

    Up limit on PDF creation

--- app/plugins/kalins-pdf-creation-station/kalins-pdf-creation-station.php
+++ app/plugins/kalins-pdf-creation-station/kalins-pdf-creation-station.php
@@ -596,7 +596,7 @@ function kalins_pdf_create_all(){
   $outputVar = new stdClass();
   kalinsPDF_createPDFDir();
 
-  $postLimit = 25;
+  $postLimit = 100;
   $postCount = 0;
 
   $myPosts = get_posts('numberposts=-1&post_type=any');
